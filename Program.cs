067228c0bdae9a12ddd319b86ab913486cc3f3eb
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-05 20:52:48 -0300

    Resolução de Exercico

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Cubatão");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Cumprida");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+        }
+    }
+}
